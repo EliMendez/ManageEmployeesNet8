8fbaa3e0e1b64c7393d33732985535dbfaa81ba0
Elizabeth Méndez <elixa_mendex@hotmail.com>
2025-03-18 09:00:55 -0600

    Agregar archivos de proyecto.

---
+++ ManageEmployeesNet8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManageEmployeesNet8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
