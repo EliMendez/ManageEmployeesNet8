f1b13b378630a17f87bcb742721c5a58cd39816c
Elizabeth Méndez <elixa_mendex@hotmail.com>
2025-03-18 10:57:54 -0600

    Create Employee

--- ManageEmployeesNet8/Controllers/EmployeeController.cs
+++ ManageEmployeesNet8/Controllers/EmployeeController.cs
@@ -1,9 +1,12 @@
 ﻿using AutoMapper;
+using ManageEmployeesNet8.Models.Dto;
+using ManageEmployeesNet8.Models;
 using ManageEmployeesNet8.Repository.Interface;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ManageEmployeesNet8.Controllers
 {
+    [Route("/[controller]")]
     public class EmployeeController : Controller
     {
         private readonly IEmployeeRepository _emRepo;
@@ -19,6 +22,28 @@ namespace ManageEmployeesNet8.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _emRepo.GetEmployees());
+        }
+
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(CreateEmployeeDto employeeDto)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var employee = _mapper.Map<Employee>(employeeDto);
+                await _emRepo.CreateEmployee(employee);
+                return RedirectToAction(nameof(Index));
+            }
+
+            //If the model is not valid, the view is returned with the errors.
+            return View(employeeDto);
+
         }
     }
 }
